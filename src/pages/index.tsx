d607f5b09df416f0584ed575ec500bc71b7c0989
Michael Avagianos <hello@maikkun.dev>
2024-08-18 22:37:16 +0300

    feat: Docusaurus project init and configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
+
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          Welcome! &#128075;
+        </Heading>
+        <p className="hero__subtitle">
+          Hi! My name is Michael, I'm a Back-end developer! I'm currently
+          studying Computer Science at Ionian University. Below you will find
+          some of my ongoing and finished projects!
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`Portfolio`} description="Portfolio page.">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
